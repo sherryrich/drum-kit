a90b83f60f0df539323009f0a6303f16acc2e53c
sherryrich <ricsherry@gmail.com>
2022-04-03 10:56:14 +0000

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # drum-kit
+
+A deployed link to the website can be found [here](https://sherryrich.github.io/drum-kit/)
+
+## Showcase
+![Preview](https://github.com/sherryrich/drum-kit/blob/main/assets/docs/drum-kit.PNG)
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,10 @@ let numberOfDrumsButtons = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfDrumsButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-     
-       this.style.color = "#fff";
-       
+
+        this.style.color = "#fff";
+
+
     });
 
 }
