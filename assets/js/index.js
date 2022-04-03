6f4d8c9dc9cc782475ed070aa3a5ce934ee0c697
sherryrich <ricsherry@gmail.com>
2022-04-03 10:23:22 +0000

    added images to html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,21 @@
 let numberOfDrumsButtons = document.querySelectorAll(".drum").length;
 
-for (let i = 0; i<numberOfDrumsButtons; i++) {
+for (let i = 0; i < numberOfDrumsButtons; i++) {
 
-document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    alert("i got clicked");
-});
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        let audio = new Audio('assets/sounds/tom-1.mp3');
+        audio.play();
+    });
 
 }
+
+// $0.addEventListener("click", function() {
+//     console.log("i got clicked");
+// });
+
+// $0.addEventListener(input1, input2);
+// function respondToClick() {
+//     console.log("I got clicked");
+// }
+
+
